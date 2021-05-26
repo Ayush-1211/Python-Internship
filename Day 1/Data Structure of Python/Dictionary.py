a2885a58a53d55027128ac939acfef18fb467fca
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-05-26 22:08:51 +0530

    Day 1 Tasks

---
+++ Day 1/Data Structure of Python/Dictionary.py
@@ -0,0 +1,8 @@
+dict = {
+  "name": "Ayush Prajapati",
+  "Age": 21,
+  "Sem": 7,
+  "college": "ASOIT"
+}
+print(dict)
+print(type(dict))
---
+++ Day 1/Data Structure of Python/Tuple.py
@@ -0,0 +1,3 @@
+tuple = ("apple", "banana", "cherry")
+print(tuple)
+print(type(tuple))
